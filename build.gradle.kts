b8435aa5927aa69ff0bbe77faed6ac400ac09dbf
aCorgi <leehae2213@gmail.com>
2025-06-24 23:48:55 +0900

    chore: gradle init

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
+    kotlin("plugin.jpa") version "1.9.25"
+    kotlin("plugin.allopen") version "1.9.25"
+    kotlin("kapt") version "1.9.25"
+
     id("org.springframework.boot") version "3.5.3"
     id("io.spring.dependency-management") version "1.1.7"
 }
@@ -9,9 +13,8 @@ group = "com.task"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
@@ -19,9 +22,23 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    runtimeOnly("com.h2database:h2")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // phone number validation
+    implementation("com.googlecode.libphonenumber:libphonenumber:9.0.5")
+
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.8")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
+
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
