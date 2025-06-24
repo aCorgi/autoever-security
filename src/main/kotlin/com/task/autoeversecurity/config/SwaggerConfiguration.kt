dfd49d422edccf8dd0c6eccbf5db7e1ee52a927b
aCorgi <leehae2213@gmail.com>
2025-06-24 23:58:38 +0900

    feat: objectMapper, Swagger configuration & application yaml 추가

---
+++ src/main/kotlin/com/task/autoeversecurity/config/SwaggerConfiguration.kt
@@ -0,0 +1,24 @@
+package com.task.autoeversecurity.config
+
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SwaggerConfiguration {
+    @Bean
+    fun openAPI(): OpenAPI {
+        return OpenAPI()
+            .components(Components())
+            .info(apiInfo())
+    }
+
+    private fun apiInfo(): Info {
+        return Info()
+            .title("보안서비스개발팀 Message Sender BE API")
+            .description("현대오토에버 보안서비스개발팀 과제 Message Sender BE API")
+            .version("0.0.1")
+    }
+}
