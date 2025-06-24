5c6afdb4e28e1556093ad8b9c84ae36d9e3c0e31
aCorgi <leehae2213@gmail.com>
2025-06-24 23:29:45 +0900

    feat: first init

---
+++ src/main/kotlin/com/task/autoeversecurity/AutoeverSecurityApplication.kt
@@ -0,0 +1,11 @@
+package com.task.autoeversecurity
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AutoeverSecurityApplication
+
+fun main(args: Array<String>) {
+    runApplication<AutoeverSecurityApplication>(*args)
+}
---
+++ src/test/kotlin/com/task/autoeversecurity/AutoeverSecurityApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.task.autoeversecurity
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AutoeverSecurityApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
