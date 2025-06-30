1b268ad7b5ddd18c4f6365dc3b96ed10e8be000c
aCorgi <leehae2213@gmail.com>
2025-06-30 20:29:12 +0900

    chore: SpEL 은 application 값에 맞게 kebab-case 로 변경

--- src/main/kotlin/com/task/autoeversecurity/component/MessageConsumer.kt
+++ src/main/kotlin/com/task/autoeversecurity/component/MessageConsumer.kt
@@ -8,13 +8,13 @@ import org.springframework.stereotype.Component
 class MessageConsumer(
     private val rabbitMQProperties: RabbitMQProperties,
 ) {
-    @RabbitListener(queues = ["\${rabbitmq.sendMessage.kakaoTalkMessageQueue.name}"])
+    @RabbitListener(queues = ["\${rabbit-mq.send-message.kakao-talk-message-queue.name}"])
     fun receiveKakaoTalkMessage(message: String) {
         println("Received message: $message")
         // 여기서 rate limiter 체크해서 처리 제한도 가능
     }
 
-    @RabbitListener(queues = ["\${rabbitmq.sendMessage.smsMessageQueue.name}"])
+    @RabbitListener(queues = ["\${rabbit-mq.send-message.sms-message-queue.name}"])
     fun receiveSmsMessage(message: String) {
         println("Received message: $message")
         // 여기서 rate limiter 체크해서 처리 제한도 가능
